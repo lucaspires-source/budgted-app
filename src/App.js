c3e86cf7194106e9fa949db84a3c1f305f94ebf2
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-08 15:04:25 -0300

    started AddTransaction Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
+import AddTransaction from './components/AddTransaction';
 const App = () => {
   return (
     <div className="container">
         <div className="app-wrapper">
           <Header/>
           <Balance/>
+          <AddTransaction/>
         </div>
     </div>
   );
--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const AddTransaction = () => {
     return (
-        <div>
-            
+        <div className="form-wrapper">
+            <form>
+                <div className="input-group income">
+                    <input type="text" placeholder="Add Income..." autoComplete="off"/>
+                    <input type="number" placeholder="Amount" autoComplete="off"/>
+                    <input type="submit" value="Submit"/>
+                </div>
+            </form>
+            <form>
+                <div className="input-group expense">
+                    <input type="text" placeholder="Add Expense..." autoComplete="off"/>
+                    <input type="number" placeholder="Amount" autoComplete="off"/>
+                    <input type="submit" value="Submit"/>
+                </div>
+            </form>
         </div>
     )
 }
