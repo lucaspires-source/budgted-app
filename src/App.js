8172e610827d7b413ff4d5adc20cd83dbeb55213
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-08 15:09:30 -0300

    started the List component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import AddTransaction from './components/AddTransaction';
+import IncomeList from './components/IncomeList';
+import ExpenseList from './components/ExpenseList'
 const App = () => {
   return (
     <div className="container">
@@ -10,6 +12,8 @@ const App = () => {
           <Header/>
           <Balance/>
           <AddTransaction/>
+          <IncomeList/>
+          <ExpenseList/>
         </div>
     </div>
   );
